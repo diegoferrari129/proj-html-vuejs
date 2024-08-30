f9806604e9b4bcc3ff06ffd8bae7ada9362f7ce4
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-08-30 23:31:31 +0200

    [UPD] created layout to AppMain.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
+import AppMain from './components/AppMain.vue';
 export default {
-  
+    components:{
+        AppMain,
+    }
 }
 </script>
 
 <template>
-
+    <AppMain />
 </template>
 
 <style lang="scss">
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,9 +5,260 @@ export default {
 </script>
 
 <template>
+    <!-- JUMBO  -->
+     <section class="jumbo">
+        
+         <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-2 offset-1">
+                    <h1>
+                        titolo
+                    </h1>
+                    <div>
+                        bottone
+                    </div>
+                </div>
+    
+            </div>
+         </div>
+     </section>
+    <!-- OUR CONSULTANTS CAN HELP YOU SECTION -->
+    <section class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <!-- section's title -->
+                <h5 class="bg-primary">OUR CONSULTANTS CAN HELP YOU</h5>
+                <div class="col-6">
+                    <!-- statistical consulting -->
+                    <div class="col-12">
+                        <div class="bg-primary me-150">
+                            statistical consulting
+                        </div>
+                    </div>
+                    <!-- banking consulting -->
+                    <div class="col-12">
+                        <div class="bg-primary me-150">
+                            banking consulting
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <!-- digital consulting -->
+                    <div class="col-12">
+                        <div class="bg-primary ms-150">
+                            digital consulting
+                        </div>
+                    </div>
+                    <!-- enterprise consulting -->
+                    <div class="col-12">
+                        <div class="bg-primary ms-150">
+                            enterprise consulting
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ABOUT US SECTION -->
+    <section class="pt-5">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-7 ps-0">
+                    
+                    <div class="bg-danger">
+                        img
+                    </div>
+                </div>
+                <div class="col-5">
+                    <div class="bg-success">
+                        about us
+                    </div>
+                </div>
+                
+            </div>
+            <div class="row">
+                <div class="col-11 ps-0">
+
+                    <div class="bg-secondary">
+                        
+
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="text-center">
+                                    text
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-center">
+                                    text
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="text-end">
+                            bottone
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    
+    <!-- PROJECT SECTION -->
+    <section class="pt-5">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-10 offset-1">
+                    <div class="row">
+                        <div class="col-6">
+
+                            <div class="bg-primary">
+                                title
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="bg-primary text-end">
+                                arrow
+                            </div>
+                        </div>
+                    </div>
+    
+                    <!-- slider -->
+                    <div class="row">
+    
+                        <div class="col-4">
+                            <div class="bg-danger">
+                                imgs
+                            </div>
+                        </div>
+    
+                        <div class="col-4">
+                            <div class="bg-danger">
+                                imgs
+                            </div>
+                        </div>
+    
+                        <div class="col-4">
+                            <div class="bg-danger">
+                                imgs
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- TESTIMONIALS SECTION-->
+    <section class="pt-5">
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-10 offset-1">
+
+                    <div class="text-end bg-primary">
+                        title
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-11 offset-1 pe-0">
+                    <div class="bg-light p-5">
+
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="bg-primary">
+                                    text
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="bg-primary">
+                                    text
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="bg-primary">
+                                    text
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- BLOG SECTION -->
+    <section class="pt-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-10 offset-1">
+                    <div class="row">
+                        <div class="d-flex justify-content-between bg-primary">
+                            <div>
+                                title
+                            </div>
+                            <div>
+                                bottone
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+        
+                        <div class="col-6">
+                            <div class="bg-danger position-relative">
+                                imgs
+                                <div class="position-absolute bg-light">
+                                    text
+                                </div>
+                            </div>
+                        </div>
+    
+                        <div class="col-6">
+                            <div class="bg-danger position-relative">
+                                imgs
+                                <div class="position-absolute bg-light">
+                                    text
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- GET STARTED SECTION -->
+    <section class="pt-5">
+        <div class="bg-danger">
+            img
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="text-center bg-primary">
+                            text
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </template>
 
 <style lang="scss" scoped>
-  
+    .me-150{
+        margin-right: 150px;
+    }
+    .ms-150{
+        margin-left: 150px;
+    }
 </style>
